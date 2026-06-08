a94ed37124a41df2207a4ed397dc944df1f8ecd6
existviber-byte <existviber@gmail.com>
2026-06-08 18:02:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,14 @@ GAME_CHAT_CHANNEL_ID = int(os.getenv('GAME_CHAT_CHANNEL_ID', 0))
 SERVER_IP = os.getenv('SERVER_IP', '37.230.137.6:20600')
 SERVER_NAME = "HOSTILE RUST | x2 | SOLO/DUO"
 
+# RCON настройки
+RCON_HOST = os.getenv('RCON_HOST', '37.230.137.6')
+RCON_PORT = int(os.getenv('RCON_PORT', 20602))
+RCON_PASSWORD = os.getenv('RCON_PASSWORD', '')
+
 # Ссылки
 VK_GROUP_URL = os.getenv('VK_GROUP_URL', 'https://vk.com/hostile_rust')
+DISCORD_INVITE = os.getenv('DISCORD_INVITE', 'https://discord.gg/invite')
 
 # ID роли администратора (опционально)
 ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID', 0))
